3a0b72d4a72a553d152f3239fc683f3f0842077c
Miguel <juanmiguelruizsupe@gmail.com>
2023-11-04 17:52:38 -0600

    enrutado

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 // const mongoose = require('mongoose');
 require('dotenv').config();
 const path = require('path');
+const bodyParser = require('body-parser');
 // const userRoute = require("./routes/user")
 
 const app = express();
 const port= process.env.PORT ||9000;
+
 
 app.use('/semantic', express.static(path.join(__dirname, '../semantic')));
 app.use(express.json());
@@ -14,21 +16,23 @@ app.use(express.json());
 app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
-app.get('/', (req, res) => {
-  const { usuario, contrasena } = req.body;
-    if (usuario === 'correo@example.com' && contrasena === 'password') {
-      res.sendFile(path.join(__dirname, 'views', 'index.html'));
-        res.json({ mensaje: 'Inicio de sesión exitoso' });
-    } else {
-        res.status(401).json({ mensaje: 'Credenciales incorrectas' });
-    }
-});
 
+app.post('/', (req, res) => {
+  const { usuario, contrasena } = req.body;
+  if (usuario === 'correo@example.com' && contrasena === 'password') {
+      res.status(401).json({ mensaje: 'Credenciales correctas' });
+  } else {
+      res.status(401).json({ mensaje: 'Credenciales incorrectas' });
+  }
+});
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
   });
 app.get('/registro.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'registro.html'));
+  });
+app.get('/productos.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'Productos.html'));
   });
 
 //mongoose  base de datos
