a172640f4457b2f39ed6018aba9bfe37a2ad9e70
Miguel <juanmiguelruizsupe@gmail.com>
2023-11-04 20:23:51 -0600

    ruta del post

--- src/index.js
+++ src/index.js
@@ -17,23 +17,25 @@ app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/registro.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'registro.html'));
+});
+app.get('/productos.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'Productos.html'));
+});
+//POST
+
 app.post('/', (req, res) => {
   const { usuario, contrasena } = req.body;
   if (usuario === 'correo@example.com' && contrasena === 'password') {
-      res.status(401).json({ mensaje: 'Credenciales correctas' });
+      res.status(200).json({ mensaje: 'Credenciales correctas' });
   } else {
       res.status(401).json({ mensaje: 'Credenciales incorrectas' });
   }
 });
-app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-  });
-app.get('/registro.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'registro.html'));
-  });
-app.get('/productos.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'Productos.html'));
-  });
 
 //mongoose  base de datos
 // mongoose.connect(process.env.MONGODB_1)
